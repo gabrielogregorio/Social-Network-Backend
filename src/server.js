2a0db540e709be89ddf3b4c467fa0a65910a257f
Gabriel Gregório <gabriel.gregorio.1@outlook.com>
2021-09-11 09:54:37 -0300

    Correção na porta de funcionamento

--- src/server.js
+++ src/server.js
@@ -1,19 +1,7 @@
 let {app, mongoose}  = require('../src/app');
-//const { Server } = require('socket.io');
 
-const server = app.listen(3333, () => {
+const PORT = process.env.PORT || 3000
+const server = app.listen(PORT, () => {
   console.log('server is running!');
 })
 
-/*
-// socket.io para tornar as coisas mais dinamicas. Ainda é preciso extruturar melhor como tudo isso vai
-acontecer
-const io = new Server(server, { cors: { origin: 'http://127.0.0.1:8080' } });
-
-io.on('connection', function(socket) {
-  console.log(socket.id)
-  socket.on('SEND_MESSAGE', function(data) {
-        io.emit('MESSAGE', data)
-    });
-})  
-*/
