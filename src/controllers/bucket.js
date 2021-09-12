1cef8e8dc365bc90315f55e622fc7a1981eadc9d
Gabriel Gregório <gabriel.gregorio.1@outlook.com>
2021-09-12 12:58:40 -0300

    Ajuste nos arquivos de configuração

--- src/controllers/bucket.js
+++ src/controllers/bucket.js
@@ -4,7 +4,7 @@ const {Storage} = require('@google-cloud/storage');
 
 const storage = new Storage({
   projectId : process.env.GCLOUD_PROJECT_ID, 
-  keyFilename : path.join(__dirname, '../../bucket-config.json')
+  keyFilename : path.join(__dirname, '../../google-credentials.json')
 }); 
 const bucket = storage.bucket(process.env.GCLOUD_STORAGE_BUCKET);
 
