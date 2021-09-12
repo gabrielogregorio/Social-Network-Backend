38b75a8ccf4a8967f9b759dc83f276a4ec1f5590
Gabriel Gregório <gabriel.gregorio.1@outlook.com>
2021-09-12 15:02:13 -0300

    Ajuste no bucket

--- src/controllers/bucket.js
+++ src/controllers/bucket.js
@@ -1,11 +1,18 @@
 require('dotenv/config');
 const path = require('path')
 const {Storage} = require('@google-cloud/storage');
+ 
 
-const storage = new Storage({
-  projectId : process.env.GCLOUD_PROJECT_ID, 
-  keyFilename : path.join(__dirname, '../../google-credentials.json')
-}); 
+let storage;
+try {
+  storage = new Storage({
+    projectId : process.env.GCLOUD_PROJECT_ID, 
+    keyFilename : path.join(__dirname, '../../google-credentials.json')
+  });   
+} catch(error) {
+  console.log('aaaaaaaaaaa')
+  console.log(error)
+}
 const bucket = storage.bucket(process.env.GCLOUD_STORAGE_BUCKET);
 
 module.exports = {bucket}
